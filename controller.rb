0a8390b4e8255c437b9dbd4f627659cbe5f01d9e
Hyunsang Han <temp@Hyunsangs-MacBookAir.local>
2018-12-30 19:51:19 +0900

    edit Docs, controller

--- controller.rb
+++ controller.rb
@@ -3,6 +3,27 @@ require './db_class.rb'
 require 'bcrypt'
 
 
+# <get>
+# 첫번호
+# 충전한내역
+# 하트쓴내역
+# 결과번호
+# 매칭상대
+# 내정보보기
+# 커트라인확정
+
+# <post>
+# 회원가입
+# 회원탈퇴
+# 로그인
+# 로그아웃
+# 하트충천
+# 하트사용
+# 랭킹만들기(나래비세우기)
+# 초대하기(공유하기)
+# 내정보수정
+# 대화방열어주기(get인가?)
+
 #Sori, Hyunsang
 post '/make_ranking' do
     user = Device.find_by_token(params["token"]).user
--- db_class.rb
+++ db_class.rb
@@ -2,59 +2,34 @@ require 'sinatra/activerecord'
 #require 'bcrypt'
 
 ############# DB definition
-# company, cash_payment, heart_payment, user_male, user_female, joined_male, joined_female, match
 
 class Company < ActiveRecord::Base
-	has_many :user_males
-    has_many :user_females
+	has_many :users
 end
 
 class CashPayment < ActiveRecord::Base
-	belongs_to :user_male #Is it right...?? (has one??)
-    belongs_to :user_female
+	belongs_to :user
 end
 
 class HeartPayment < ActiveRecord::Base
-	belongs_to :user_male
-    belongs_to :user_female
+	belongs_to :user
 end
 
-class UserMale < ActiveRecord::Base
+class User < ActiveRecord::Base
+    belongs_to :company
     has_many :devices
     has_many :cash_payments
-    has_many :used_hearts
-    has_many :joined_males #Is it right...?? (has one??)
-    has_many :matches, through :joined_male
+    has_many :heart_payments
+    has_many :joined_users
 end
 
-class UserFemale < ActiveRecord::Base
-	has_many :devices
-    has_many :cash_payments
-    has_many :used_hearts
-    has_many :joined_females
-    has_many :matches, through :joined_female
-end
+class JoinedUser < ActiveRecord::Base
+	belongs_to :user
 
-class JoinedMale < ActiveRecord::Base
-	belongs_to :user_male
-    has_one :match
-    has_one :joined_male, through :match
-end
-
-class JoinedFemale < ActiveRecord::Base
-	belongs_to :user_female
-    has_one :match
-    has_one :joined_female, through :match
-end
-
-class Match < ActiveRecord::Base
-	has_many :joined_males
-    has_many :joined_females
-    has_many :user_males, through :joined_male
-    has_many :user_females, through :joined_female
+class MatchedHistory < ActiveRecord::Base
+	belongs_to :user
 end
 
 class Device < ActiveRecord::Base
-	belongs_to :user_male
-    belongs_to :user_female
+	belongs_to :user
 end
